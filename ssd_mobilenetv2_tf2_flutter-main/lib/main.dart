2d2aa609a19e8ed047fd89471c08a6166cba7c9f
HuongNTY <86402070+HuongNTY@users.noreply.github.com>
2022-01-05 10:29:43 +0700

    Add files via upload

---
+++ ssd_mobilenetv2_tf2_flutter-main/lib/main.dart
@@ -0,0 +1,249 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as img;
+
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_example/realtime//live_camera.dart';
+import 'package:camera/camera.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+
+  runApp(new App());
+}
+
+List<CameraDescription> cameras;
+var detectedObj = [];
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  File _image;
+  List _recognitions;
+  double _imageHeight;
+  double _imageWidth;
+  bool _busy = false;
+
+  Future predictImagePicker() async {
+    final picker  = ImagePicker();
+    //var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    var image = await picker.pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+    setState(() {
+      _image = File(image.path);
+      _busy = true;
+    });
+    predictImage(_image);
+  }
+
+  Future predictImageCamera() async {
+    final picker  = ImagePicker();
+
+    var image = await picker.pickImage(source: ImageSource.camera);
+
+    if (image == null) return;
+    setState(() {
+      _image = File(image.path);
+      _busy = true;
+    });
+    predictImage(_image);
+  }
+
+  Future predictImage(File image) async {
+    if (image == null) return;
+
+    await ssdMobileNet(image);
+
+    new FileImage(image)
+        .resolve(new ImageConfiguration())
+        .addListener(ImageStreamListener((ImageInfo info, bool _) {
+      setState(() {
+        _imageHeight = info.image.height.toDouble();
+        _imageWidth = info.image.width.toDouble();
+      });
+    }));
+
+    setState(() {
+      _image = image;
+      _busy = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _busy = true;
+
+    loadModel().then((val) {
+      setState(() {
+        _busy = false;
+      });
+    });
+  }
+
+  Future loadModel() async {
+    Tflite.close();
+    try {
+      String res;
+      res = await Tflite.loadModel(
+        model: "assets/ssd_mobilenet.tflite",
+        labels: "assets/ssd_mobilenet.txt",
+          );
+      print(res);
+    } on PlatformException {
+      print('Failed to load model.');
+    }
+  }
+
+  Future ssdMobileNet(File image) async {
+    int startTime = new DateTime.now().millisecondsSinceEpoch;
+    var recognitions = await Tflite.detectObjectOnImage(
+      path: image.path,
+      numResultsPerClass: 1,
+      threshold: 0.3,
+    );
+
+    setState(() {
+      _recognitions = recognitions;
+    });
+    int endTime = new DateTime.now().millisecondsSinceEpoch;
+    print("Inference took ${endTime - startTime}ms");
+  }
+
+  List<Widget> renderBoxes(Size screen) {
+    if (_recognitions == null) return [];
+    if (_imageHeight == null || _imageWidth == null) return [];
+
+    double factorX = screen.width;
+    double factorY = _imageHeight / _imageWidth * screen.width;
+    Color blue = Color.fromRGBO(37, 213, 253, 1.0);
+    detectedObj.clear();
+
+    return _recognitions.map((re) {
+      detectedObj.add("${re["detectedClass"]} ${(re["confidenceInClass"] * 100).toStringAsFixed(0)}%");
+      return Positioned(
+        left: re["rect"]["x"] * factorX,
+        top: re["rect"]["y"] * factorY,
+        width: re["rect"]["w"] * factorX,
+        height: re["rect"]["h"] * factorY,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            border: Border.all(
+              color: blue,
+              width: 2,
+            ),
+          ),
+          child: Text(
+            "${re["detectedClass"]} ${(re["confidenceInClass"] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+              background: Paint()..color = blue,
+              color: Colors.white,
+              fontSize: 12.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  List<Widget> objList(List list) {
+    if (list.isEmpty) return[];
+    var lists = <Widget>[];
+    double pos = 0;
+    for(int i=0; i<list.length; i++)
+      {
+        lists.add(Positioned(
+          left: 5,
+          top: 20*pos,
+          child: Text("${list[i]}",
+              style: TextStyle(
+          color: Colors.red,
+          fontSize: 16.0)
+        )
+        )
+        );
+        pos+=1;
+      }
+    return lists;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    List<Widget> stackChildren = [];
+
+    stackChildren.add(Positioned(
+      top: 0.0,
+      left: 0.0,
+      width: size.width,
+      child: _image == null ? Text('No image selected.') : Image.file(_image),
+    ));
+
+    stackChildren.addAll(renderBoxes(size));
+    stackChildren.addAll(objList(detectedObj));
+
+    if (_busy) {
+      stackChildren.add(const Opacity(
+        child: ModalBarrier(dismissible: false, color: Colors.grey),
+        opacity: 0.3,
+      ));
+      stackChildren.add(const Center(child: CircularProgressIndicator()));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('tflite example app'),
+      ),
+      body: Stack(
+        children: stackChildren,
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget> [
+          FloatingActionButton(
+            onPressed: predictImagePicker,
+            tooltip: 'Pick Image',
+            child: Icon(Icons.image),
+          ),
+          SizedBox(width: 10,),
+          FloatingActionButton(
+            onPressed: predictImageCamera,
+            tooltip: 'Take Photo Image',
+            child: Icon(Icons.camera_alt),
+          ),
+          SizedBox(width: 10,),
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => LiveFeed(cameras),
+              ),
+              );
+            },
+            tooltip: 'Real Time Detection',
+            child: Icon(Icons.video_camera_back),
+          ),
+        ],
+      )
+    );
+  }
+}
